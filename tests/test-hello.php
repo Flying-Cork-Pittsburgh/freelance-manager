e0ca29cfdc3743ec89cc0b11d5afbf69284e9d43
Andrew Woods <atwoods1@gmail.com>
2015-06-22 09:52:43 -0700

    Added phpunit testing framework using WP-CLI.

---
+++ tests/test-hello.php
@@ -0,0 +1,19 @@
+<?php
+
+class HelloTest extends WP_UnitTestCase {
+
+	public function __construct() {
+		// do something;
+	}
+
+	function testFirst() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+
+	function test_second() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
