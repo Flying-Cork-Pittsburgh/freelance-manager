37ef3041425727bd376f4a78ecb9def59d1cefdb
Andrew Woods <atwoods1@gmail.com>
2015-07-01 20:33:47 -0700

    Added functionality for 'wp client add'
    Processes the $args and $assoc_args.
    Uses the Client_Admin class to update database.

--- wpcli_client.php
+++ wpcli_client.php
@@ -7,21 +7,82 @@
 class Client_Command extends WP_CLI_Command {
 
     /**
-     * Add a client.
+     * Add a Client.
      *
      * ## OPTIONS
      *
      * <name>
-     * : The client you want to create.
+     * :   The name of the Client
+     *
+     * <slug>
+     * :   The unique value that can used in an URL.
      *
      * ## EXAMPLES
+	 *
+     * wp client add Company
+     * wp client add 'Company Name' [company-name]
      *
-     *     wp client add Newman
-     *
-     * @synopsis <name>
+     * @synopsis <name> [<slug>] [--location=<location>] [--website=<url>] [--phone=<tel>] [--contact_name=<fullname>] [--contact_email=<email>]
      */
-    function add( $args, $assoc_args ) {
-        list( $name ) = $args;
+    function add( $args, $assoc_args = array() ) {
+		$data = array();
+		$data['location'] = '';
+		$data['website'] = '';
+		$data['phone'] = '';
+		$data['person'] = '';
+		$data['email'] = '';
+		$data['sha1'] = '';
+
+		$name = $args[0];
+		$slug = ( isset( $args[1] ) ) ? sanitize_title( $args[1] ) : sanitize_title( $name );
+
+		$post = array();
+		$post['post_title'] = $name;
+		$post['post_name'] = $slug;
+		$post['post_type'] = 'client';
+		$post['post_author'] = '1';
+
+		$post_id = wp_insert_post( $post, true );
+		if( is_wp_error( $post_id ) ) {
+			error_log( print_r( $post_id->get_error_message(), true ) );
+			return;
+		}
+
+		$client_admin = new Client_Admin();
+
+		error_log( 'assoc_args=' . print_r( $assoc_args, true ) );
+
+		if ( isset( $assoc_args['location'] ) ){
+			$data['location'] = sanitize_text_field( $assoc_args['location'] );
+		}
+		if ( isset( $assoc_args['url']) ){
+			$data['website']  = sanitize_text_field( $assoc_args['url'] );
+		}
+		if ( isset($assoc_args['tel']) ) {
+			$data['phone']    = sanitize_text_field( $assoc_args['tel'] );
+		}
+
+		if ( isset($assoc_args['fullname']) ) {
+			$data['person'] = sanitize_text_field( $assoc_args['fullname'] );
+		}
+
+		if ( isset($assoc_args['email']) ) {
+			$data['email'] = sanitize_text_field( $assoc_args['email'] );
+		}
+
+		if ( $post_id && $name &&
+			$data['website'] && $data['person'] && $data['email'] ) {
+
+			$data['sha1'] = $this->create_sha(
+				$post_id,
+				$name,
+				$data['website'],
+				$data['person'],
+				$data['email']
+			);
+		}
+
+		$client_admin->update_meta( $post_id, $data );
 
         // Print a success message
         WP_CLI::success( "todo: Added $name!" );
