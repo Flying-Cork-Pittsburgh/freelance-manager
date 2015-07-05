67b6ef23e05154e237de3501ec29e4ddab27c1f0
Andrew Woods <atwoods1@gmail.com>
2015-07-04 20:41:20 -0700

    Added tests - test_get_field_info() and test_delete_meta()

--- tests/test-client-admin.php
+++ tests/test-client-admin.php
@@ -2,10 +2,6 @@
 
 class ClientAdminTest extends WP_UnitTestCase {
 
-	public function __construct() {
-
-	}
-
 	function test_column_data() {
 		$data = [
 			'first' => 'First',
@@ -24,6 +20,31 @@ class ClientAdminTest extends WP_UnitTestCase {
 
 		$this->assertArrayHasKey('location', $data);
 		$this->assertArrayNotHasKey('author', $data);
+	}
+
+	public function test_get_field_info() {
+
+		$client_admin = new Client_Admin();
+
+		$this->assertEquals( '_client_location', $client_admin->get_field( 'location' ) );
+		$this->assertEquals( '_client_website', $client_admin->get_field( 'website' ) );
+		$this->assertEquals( '_client_phone', $client_admin->get_field( 'phone' ) );
+		$this->assertEquals( '_client_sha1', $client_admin->get_field( 'sha' ) );
+		$this->assertEquals( '_client_contact_person', $client_admin->get_field( 'contact_name' ) );
+		$this->assertEquals( '_client_contact_email', $client_admin->get_field( 'contact_email' ) );
+		$this->assertFalse( $client_admin->get_field( 'does_not_exist' ));
+	}
+
+	public function test_delete_meta() {
+		$data = array( 'phone' => 1, 'sha' => 2 );
+		$post_id = 1;
+
+		$client_admin = new Client_Admin();
+		$updated = $client_admin->update_meta( $post_id, $data );
+		$this->assertEquals( $updated, 2 );
+
+		$deleted = $client_admin->delete_meta( $post_id, $data );
+		$this->assertEquals( $deleted, 2 );
 	}
 
 }
