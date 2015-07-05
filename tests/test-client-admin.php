4ad864f79189661481cc0f915d3382c66db41797
Andrew Woods <atwoods1@gmail.com>
2015-07-05 15:53:57 -0700

    Added tests for Client_Admin::create_sha()

--- tests/test-client-admin.php
+++ tests/test-client-admin.php
@@ -47,5 +47,30 @@ class ClientAdminTest extends WP_UnitTestCase {
 		$this->assertEquals( $deleted, 2 );
 	}
 
+	public function test_create_sha(){
+		$post_id = 1;
+		$website = 'http://example.com';
+		$person  = 'Firstname Lastname';
+		$email   = 'firstname_lastname@example.com';
+
+		$client_admin = new Client_Admin();
+		$example_sha1 = $client_admin->create_sha($post_id, $website, $person,  $email);
+		$this->assertEquals(  $example_sha1, '051458329c9600e035c086e591ce8d79668647b8' );
+
+		$website_sha1 = $client_admin->create_sha($post_id, 'http://example.org', $person,  $email);
+		$this->assertEquals( $website_sha1, '7bb2f808d9ddc9525433d38d93050f77be4833c0' );
+		$this->assertNotEquals( $website_sha1, $example_sha1 );
+
+		$person_sha1 = $client_admin->create_sha($post_id, $website, 'Andrew Woods',  $email);
+		$this->assertEquals( $person_sha1, 'deba213f1be477d3fa0081f1f5179caf63d3bb7d' );
+		$this->assertNotEquals( $person_sha1, $example_sha1 );
+		$this->assertNotEquals( $person_sha1, $website_sha1 );
+
+		$email_sha1 = $client_admin->create_sha($post_id, $website, $person, 'awoods@example.com' );
+		$this->assertEquals( $email_sha1, '4887dfb31938fec072c3eeca051ccfd1559cc334' );
+		$this->assertNotEquals( $email_sha1, $example_sha1 );
+		$this->assertNotEquals( $email_sha1, $website_sha1 );
+		$this->assertNotEquals( $email_sha1, $person_sha1 );
+	}
 }
 
